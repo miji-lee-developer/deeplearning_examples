353b581eb7eb9f7cb4bf3dc1512a4895030535e2
miji <miji.lee.developer@gmail.com>
2020-10-16 22:54:30 +0900

    Day 20. char seq rnn

--- ex_tf_char_seq_rnn.py
+++ ex_tf_char_seq_rnn.py
@@ -34,9 +34,11 @@ tf.model.add(tf.keras.layers.LSTM(units=num_classes, input_shape=(sequence_lengt
 tf.model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units=num_classes, activation='softmax')))
 tf.model.summary()
 tf.model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(lr=learning_rate), metrics=['accuracy'])
-tf.model.fit(x_one_hot_eager, y_one_hot_eager, steps_per_epoch=5000)
+# tf.model.fit(x_one_hot_eager, y_one_hot_eager, steps_per_epoch=5000)
+tf.model.fit(x_one_hot_numpy, y_one_hot_eager, steps_per_epoch=5000)
 
-predictions = tf.model.predict(x_one_hot_eager, steps=1)
+# predictions = tf.model.predict(x_one_hot_eager, steps=1)
+predictions = tf.model.predict(x_one_hot_numpy, steps=1)
 
 for i, prediction in enumerate(predictions):
     # print char using argmax, dict
