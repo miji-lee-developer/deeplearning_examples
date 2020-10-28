4dc2271479ac342ec1dfa10b8531ce46b9179068
miji <miji.lee.developer@gmail.com>
2020-10-28 20:52:20 +0900

    Day 32. one hot encoding test

---
+++ test.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+# y_one_hot = tf.keras.utils.to_categorical(y_data, num_classes=3)
+data = pd.read_csv("./input/Titanic/train.csv", index_col="PassengerId")
+print(data.shape)
+data.head()
+
+# print(data[["Embarked"]].head())
+# embarked_one_hot = tf.keras.utils.to_categorical(data["Embarked"], num_classes=3)
+
+d = data["Embarked"]
+print(type(d))
+
+# t = tf.keras.preprocessing.text.Tokenizer()
+# t.fit_on_sequences(data["Embarked"])
+# print(t.word_index)
