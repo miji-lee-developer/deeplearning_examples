e976a4eb7f8fbeda46469397471669babae34425
miji <miji.lee.developer@gmail.com>
2020-10-31 20:08:26 +0900

    Day 35. one hot encoding test

--- test.py
+++ test.py
@@ -15,3 +15,11 @@ print(data[["Embarked", "Embarked_num"]])
 
 one_hot = tf.keras.utils.to_categorical(data.Embarked_num, num_classes=len(d.items()))
 print(one_hot)
+
+df = pd.DataFrame(one_hot)
+data["Embarked_S"] = df[0]
+data["Embarked_C"] = df[1]
+data["Embarked_Q"] = df[2]
+data["Embarked_Null"] = df[3]
+
+print(data[["Embarked","Embarked_C","Embarked_S","Embarked_Q","Embarked_Null"]].head())
